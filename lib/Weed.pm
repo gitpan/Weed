cc4f24539223dda4f9271619ba7900e38bbf9bdc
Hon Wejun <hooo@cpan.org>
2007-04-20 21:14:26 +0000

    Import of HOOO/Weed-0.0003 from CPAN.
    gitpan-cpan-distribution: Weed
    gitpan-cpan-version:      0.0003
    gitpan-cpan-path:         HOOO/Weed-0.0003.tar.gz
    gitpan-cpan-author:       HOOO
    gitpan-cpan-maturity:     released

---
+++ lib/Weed.pm
@@ -0,0 +1,39 @@
+package Weed;
+use strict;
+use warnings;
+
+our $VERSION = '0.0003';
+
+=head1 NAME
+
+Weed - ist ein englisches Wort für Unkraut
+
+=head1 ÜBERSICHT
+
+	use Weed;
+
+=head1 BAUM
+
+-+- L<Weed>
+
+=head1 SIEHE AUCH
+
+L<Math::Vectors>
+
+L<Weed::Object>
+
+L<Weed::Field>, L<Weed::ArrayField>
+
+=head1 AUTHOR
+
+Holger Seelig  holger.seelig@yahoo.de
+
+=head1 COPYRIGHT
+
+Das ist freie Software; du kannsts sie weiter verteilen und/oder verändern
+nach den gleichen Bedingungen wie L<Perl|perl> selbst.
+
+=cut
+
+1;
+__END__
---
+++ t/01_weed.t
@@ -0,0 +1,12 @@
+#!/usr/bin/perl -w
+use Test::More tests => 1;
+use strict;
+
+BEGIN {
+	$| = 1;
+	chdir 't' if -d 't';
+	unshift @INC, '../lib';
+	use_ok('Weed');
+}
+
+__END__
